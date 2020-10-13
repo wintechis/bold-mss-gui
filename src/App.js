10ae90b8c131feb0ebe41056b7f6a6efec2bf008
Daniel Schraudner <daniel.schraudner@fau.de>
2020-10-13 12:06:06 +0200

    Added stations and links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Group, Stage, Layer, Rect, Line, Circle } from 'react-konva';
 
+const baseURI = 'https://localhost:8443/arena2036/'
+
 class Grid extends React.Component {
   render() {
     let lines = []
@@ -16,8 +18,30 @@ class Grid extends React.Component {
   }
 }
 
+class Station extends React.Component {
+  render() {
+    let uri = baseURI + 'stations/' + this.props.type + '/' + this.props.entity + '/#station'
+    return (
+      <Group>
+        <Rect
+          x={this.props.x * 50}
+          y={this.props.y * 50}
+          width={this.props.width * 50}
+          height={this.props.height * 50}
+          fill='darkgreen'
+          stroke='black'
+          onMouseEnter={() => this.props.setHoverUri(uri)}
+          onMouseLeave={() => this.props.setHoverUri('')}
+          onMouseUp={() => window.open(uri, '_blank')}
+        />
+      </Group>
+    )
+  }
+}
+
 class Forklift extends React.Component {
   render() {
+    let uri = baseURI + 'forklifts/' + this.props.entity + '/#fl'
     return (
       <Group>
         <Rect
@@ -27,6 +51,9 @@ class Forklift extends React.Component {
           height={50}
           fill='#bbbbbb'
           stroke='black'
+          onMouseEnter={() => this.props.setHoverUri(uri)}
+          onMouseLeave={() => this.props.setHoverUri('')}
+          onMouseUp={() => window.open(uri, '_blank')}
         />
         <Circle
           x={this.props.x * 50 + 25}
@@ -34,6 +61,9 @@ class Forklift extends React.Component {
           radius={18}
           fill='#e4e4e4'
           stroke='black'
+          onMouseEnter={() => this.props.setHoverUri(uri)}
+          onMouseLeave={() => this.props.setHoverUri('')}
+          onMouseUp={() => window.open(uri, '_blank')}
         />
       </Group>
     )
@@ -44,8 +74,11 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      forklifts: new Map()
+      hover: 'https://test.org/asdasd',
+      forklifts: new Map(),
+      stations: new Map()
     }
+    this.setHoverUri = this.setHoverUri.bind(this);
   }
 
   componentDidMount() {
@@ -60,27 +93,72 @@ class App extends Component {
         this.setState({
           forklifts: m
         })
+      } else if(data.type === 'station') {
+        let m = this.state.stations
+        let s = Object.assign(m.get(data.id) || {}, data)
+        m.set(data.id, s)
+        this.setState({
+          stations: m
+        })
       }
       this.forceUpdate()
     }
   }
 
+  setHoverUri(uri) {
+    this.setState({
+      hover: uri
+    })
+  }
+
   render() {
-    let forklifts = Array.from(this.state.forklifts).map(([_,fl]) => <Forklift key={fl.id} entity={fl.id} x={fl.x} y={fl.y} />)
+    let forklifts = Array.from(this.state.forklifts).map(([_,fl]) => <Forklift
+      key={fl.id}
+      entity={fl.id}
+      x={fl.x}
+      y={fl.y}
+      setHoverUri={this.setHoverUri}
+    />)
+    let stations = Array.from(this.state.stations).map(([_,s]) => <Station
+      key={s.id}
+      entity={s.id}
+      type={s.stationType}
+      x={s.x}
+      y={s.y}
+      width={s.width}
+      height={s.height}
+      setHoverUri={this.setHoverUri}
+    />)
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"}}
-      >
-        <Stage width={1002} height={1002} offsetX={-1} offsetY={-1} style={{margin: '50px'}}>
-          <Grid />
-          <Layer>
-            {forklifts}
-          </Layer>
-        </Stage>
-      </div>
+      <>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Stage width={1002} height={1002} offsetX={-1} offsetY={-1} style={{margin: '50px'}}>
+            <Grid />
+            <Layer>
+              {stations}
+            </Layer>
+            <Layer>
+              {forklifts}
+            </Layer>
+          </Stage>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "20pt"
+          }}
+        >
+          {this.state.hover}
+        </div>
+      </>
     );
   }
 }
